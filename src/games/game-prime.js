865856705063236bf86d6fd1c3158505fcc0aac8
NettaK0t <katt.laps@mail.ru>
2022-12-08 10:01:22 +0300

    add brain-prime logic

---
+++ src/games/game-prime.js
@@ -0,0 +1,24 @@
+import playGame from '../index.js';
+import getRandomNumber from '../getRandomNumber.js';
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+    if (num <= 1) return false;
+    for (let i = 2; i < (num / 2 + 1); i += 1){
+        if (num % i === 0) return false;
+    };
+    return true;
+};
+
+const getQuestionAndAnswer = () => {  
+    const question = getRandomNumber(0,179);;
+    const answer = isPrime(question) ? 'yes' : 'no';
+    return [question, String(answer)];
+};
+
+const initGamePrime = () => {
+    playGame(rule, getQuestionAndAnswer);
+  };
+  
+  export default initGamePrime;
