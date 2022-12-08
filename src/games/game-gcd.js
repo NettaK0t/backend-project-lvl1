0d545b49ebcaee059e270031a348f7904bed79ea
NettaK0t <katt.laps@mail.ru>
2022-12-08 11:41:27 +0300

    fix brain-gcd

--- src/games/game-gcd.js
+++ src/games/game-gcd.js
@@ -4,13 +4,11 @@ import getRandomNumber from '../getRandomNumber.js';
 const rule = 'Find the greatest common divisor of given numbers.';
 
 const getGCD = (num1, num2) => {
-  const minNum = num1 > num2 ? num2 : num1;
-  let gcd = 1;
-  for (let i = 2; i <= minNum; i += 1) {
-    if (num1 % i === 0 && num2 % i === 0) gcd = i;
-  }
-
-  return gcd;
+  if (num2) {
+    return getGCD(num2, num1 % num2);
+} else {
+    return Math.abs(num1);
+}
 };
 
 const getQuestionAndAnswer = () => {
